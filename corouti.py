464f6a3d47c9a7b78081c0e9f4f07df5e81ee821
yogeshsaravanan <yogesh41529@gmail.com>
2024-05-20 08:02:25 +0530

    updated async

--- corouti.py
+++ corouti.py
@@ -1,20 +1,33 @@
 import time
+import asyncio
 
-def make_cofee():
+# importing asyncio to run this functions parallely
+
+async def make_cofee():
     print('started preparing cofee')
-    time.sleep(3)
+    await asyncio.sleep(3)
     print('cofee is ready')
+    # return 1
     
-def boil_egg():
+async def boil_egg():
     print('started preparing egg')
-    time.sleep(2)
+    await asyncio.sleep(2)
     print('egg is ready')
+    # return 2
     
-if __name__ == '__main__':
+async def main():
     start_time=time.time()
-    make_cofee()
-    boil_egg()
+    # make_cofee()
+    # boil_egg()
+    # gather the fnctions which we are going to run
+    
+    batch = asyncio.gather(make_cofee(),boil_egg())
+    await batch
+    # re1,re2 = await batch
     endtime =time.time()-start_time
     print('execution time: ',endtime)
+    
+if __name__ == '__main__':
+    asyncio.run(main())
     
     
